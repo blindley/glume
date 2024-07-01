250dd1a85eedeb774b2b5ae2a09146287c018bad
Benjamin Lindley <benjameslindley@gmail.com>
2024-07-01 09:52:46 -0500

    Window struct

--- src/window.rs
+++ src/window.rs
@@ -45,104 +45,119 @@ pub enum Event {
     KeyReleased(VirtualKeyCode),
 }
 
-pub fn run<F>(window_settings: WindowSettings, event_handler: F) -> !
-where
-    F: 'static + FnMut(&mut WindowController, Event) -> Result<(), Box<dyn std::error::Error>>
-{
-    let mut event_handler = event_handler;
+pub struct Window {
+    event_loop: EventLoop<()>,
+    windowed_context: glutin::WindowedContext<glutin::PossiblyCurrent>,
+}
 
-    let el = EventLoop::new();
-    let wb = WindowBuilder::new();
-    let wb = wb.with_title(window_settings.title);
+impl Window {
+    pub fn new(window_settings: WindowSettings) -> Self {
+        let el = EventLoop::new();
+        let wb = WindowBuilder::new();
+        let wb = wb.with_title(window_settings.title);
 
-    let inner_size = glutin::dpi::LogicalSize::new(window_settings.size.0, window_settings.size.1);
-    let wb = wb.with_inner_size(inner_size);
+        let inner_size = glutin::dpi::LogicalSize::new(window_settings.size.0, window_settings.size.1);
+        let wb = wb.with_inner_size(inner_size);
 
-    let windowed_context = ContextBuilder::new();
-    let windowed_context = windowed_context.with_gl_profile(glutin::GlProfile::Core);
-    let windowed_context = windowed_context.with_gl(glutin::GlRequest::Specific(
-        glutin::Api::OpenGl,
-        window_settings.gl_version,
-    ));
+        let windowed_context = ContextBuilder::new();
+        let windowed_context = windowed_context.with_gl_profile(glutin::GlProfile::Core);
+        let windowed_context = windowed_context.with_gl(glutin::GlRequest::Specific(
+            glutin::Api::OpenGl,
+            window_settings.gl_version,
+        ));
 
-    let windowed_context = windowed_context.build_windowed(wb, &el).unwrap();
-    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+        let windowed_context = windowed_context.build_windowed(wb, &el).unwrap();
+        let windowed_context = unsafe { windowed_context.make_current().unwrap() };
 
-    gl::load_with(|s| windowed_context.get_proc_address(s) as *const _);
+        gl::load_with(|s| windowed_context.get_proc_address(s) as *const _);
 
-    let exit = {
-        let mut wc = WindowController {
-            exit: false,
-            windowed_context: &windowed_context,
-        };
-
-        if let Err(e) = event_handler(&mut wc, Event::WindowInitialized) {
-            eprintln!("Error: {}", e);
-            wc.exit = true;
+        Self {
+            event_loop: el,
+            windowed_context,
         }
-
-        wc.exit
-    };
-
-    if exit {
-        std::process::exit(0);
     }
 
-    el.run(move |event, _, control_flow| {
-        *control_flow = ControlFlow::Wait;
-        let mut wc = WindowController {
-            exit: false,
-            windowed_context: &windowed_context,
+    
+    pub fn run<F>(self, event_handler: F) -> !
+    where
+        F: 'static + FnMut(&mut WindowController, Event) -> Result<(), Box<dyn std::error::Error>>
+    {
+        let mut event_handler = event_handler;
+
+        let exit = {
+            let mut wc = WindowController {
+                exit: false,
+                windowed_context: &self.windowed_context,
+            };
+
+            if let Err(e) = event_handler(&mut wc, Event::WindowInitialized) {
+                eprintln!("Error: {}", e);
+                wc.exit = true;
+            }
+
+            wc.exit
         };
 
-        use glutin::event::Event as Ev;
-        use glutin::event::WindowEvent as WinEv;
-        use glutin::event::ElementState;
+        if exit {
+            std::process::exit(0);
+        }
 
-        let result = match event {
-            Ev::LoopDestroyed => Ok(()),
-            Ev::WindowEvent { event, .. } => match event {
-                WinEv::Resized(physical_size) => {
-                    windowed_context.resize(physical_size);
-                    event_handler(&mut wc, Event::Resized(physical_size.into()))
-                }
+        self.event_loop.run(move |event, _, control_flow| {
+            *control_flow = ControlFlow::Wait;
+            let mut wc = WindowController {
+                exit: false,
+                windowed_context: &self.windowed_context,
+            };
 
-                WinEv::CloseRequested => {
-                    wc.exit = true;
-                    event_handler(&mut wc, Event::CloseRequested)
-                }
+            use glutin::event::Event as Ev;
+            use glutin::event::WindowEvent as WinEv;
+            use glutin::event::ElementState;
 
-                WinEv::KeyboardInput { input, .. } => {
-                    if let Some(vk) = input.virtual_keycode {
-                        match input.state {
-                            ElementState::Pressed => event_handler(&mut wc, Event::KeyPressed(vk)),
-                            ElementState::Released => event_handler(&mut wc, Event::KeyReleased(vk)),
-                        }
-                    } else {
-                        Ok(())
+            let result = match event {
+                Ev::LoopDestroyed => Ok(()),
+                Ev::WindowEvent { event, .. } => match event {
+                    WinEv::Resized(physical_size) => {
+                        self.windowed_context.resize(physical_size);
+                        event_handler(&mut wc, Event::Resized(physical_size.into()))
                     }
+
+                    WinEv::CloseRequested => {
+                        wc.exit = true;
+                        event_handler(&mut wc, Event::CloseRequested)
+                    }
+
+                    WinEv::KeyboardInput { input, .. } => {
+                        if let Some(vk) = input.virtual_keycode {
+                            match input.state {
+                                ElementState::Pressed => event_handler(&mut wc, Event::KeyPressed(vk)),
+                                ElementState::Released => event_handler(&mut wc, Event::KeyReleased(vk)),
+                            }
+                        } else {
+                            Ok(())
+                        }
+                    },
+                    _ => Ok(()),
                 },
+
+                Ev::RedrawRequested(_) => {
+                    event_handler(&mut wc, Event::RedrawRequested)
+                        .and_then(|_| {
+                            self.windowed_context.swap_buffers()?;
+                            Ok(())
+                        })
+                },
+
                 _ => Ok(()),
-            },
+            };
 
-            Ev::RedrawRequested(_) => {
-                event_handler(&mut wc, Event::RedrawRequested)
-                    .and_then(|_| {
-                        windowed_context.swap_buffers()?;
-                        Ok(())
-                    })
-            },
+            if let Err(e) = result {
+                eprintln!("Error: {}", e);
+                wc.exit = true;
+            }
 
-            _ => Ok(()),
-        };
-
-        if let Err(e) = result {
-            eprintln!("Error: {}", e);
-            wc.exit = true;
-        }
-
-        if wc.exit {
-            *control_flow = ControlFlow::Exit;
-        }
-    });
+            if wc.exit {
+                *control_flow = ControlFlow::Exit;
+            }
+        });
+    }
 }
